32c69d1465985292e2b879576e2af0e55e3ff559
Natalia Terlecka <natalia@wehavesun.com>
2024-04-22 12:46:50 +0100

    first commit

---
+++ Now/NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowApp.swift
+//  Now
+//
+//  Created by Natalia Terlecka on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AnimatedGradientCircleWithCountdown()
+        }
+    }
+}
